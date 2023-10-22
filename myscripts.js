0402c8ea0b202737ea8a530428f30b3d92432f5e
yuliiapodlutskaia <yulechka-ya-2015@mail.ru>
2023-10-22 23:14:37 +0800

    first commit

---
+++ myscripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function (){
+
+    $('.burger-menu').click(function(){
+
+        $('.header__menu-items').css('display','flex');
+        $('.burger-menu').css('display','none');
+        $('.close-logo').css('display','flex');
+    })
+    $('#close-logo').click(function(){
+        $('#menu-items').css('display','none');
+        $('#close-logo').css('display','none');
+        $('#burger-menu').css('display','flex');
+    })
+
+    $('.carousel').carousel({
+        dots:true,
+        arrow:false
+        }
+    )
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: true,
+        time: 100000000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+
+    })
+});
